909dd580abf2b863b8c3e53648b4a80b714ef323
Charmane Mchunu <charmane.mchunu@sambeconsulting.com>
2026-04-06 16:07:36 +0200

    Did control flow and functions, chatbot and rock paper scissors

---
+++ Python For Beginners/Udemy Mini Projects/ChatBot.py
@@ -0,0 +1,28 @@
+import sys
+from datetime import datetime
+
+def get_response(text: str) -> str :
+    lowered: str = text.lower()
+    if lowered in ['hello','hi','hey']:
+        return 'Hello there'
+    elif 'how are you' in lowered:
+        return 'I\'m good thanks!'
+    elif 'your name' in lowered:
+        return 'My name is : Bot :)'
+    elif 'time' in lowered:
+        current_time = datetime.now()
+        return f'The time is {current_time.strftime("%H:%M")}'
+    elif lowered in ['bye','see you','goodbye']:
+        return 'It was nce talking to you! Bye!'
+    else:
+        return f'Sorry, I do not understand:"{text}".'
+
+while True:
+    user_input: str = input("You : ")
+
+    if user_input == 'exit':
+        print('Bot : It was a pleasure talking to you!')
+        sys.exit()
+
+    bot_response: str = get_response(user_input)
+    print(f'Bot: {bot_response}')
---
+++ Python For Beginners/Udemy Mini Projects/SimpleAdderProject.py
@@ -0,0 +1,7 @@
+print("Welcome to your simple adder!")
+print("---------------------------------------------")
+j: str =(input("Enter a value for j :"))
+k: str = input("Enter a value for k :")
+
+print("-----------------------------------------------")
+print("The result is :",int(j) + int(k))
