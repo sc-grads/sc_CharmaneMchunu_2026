7a3c248135e5c9d349a96b105f120f4329f21f8d
charmane-mchunu <charmane.mchunu@sambeconsulting.com>
2026-07-08 12:11:43 +0200

    check existence before creation

--- Database/Triggers.sql
+++ Database/Triggers.sql
@@ -1,9 +1,3 @@
-USE TimesheetDB;
-GO
-
-/* ============================================================
-   1. LeaveCategory column fix
-============================================================ */
 IF NOT EXISTS (
     SELECT 1 FROM sys.columns
     WHERE object_id = OBJECT_ID('dbo.LeaveCategory')
@@ -11,166 +5,176 @@ IF NOT EXISTS (
 )
 BEGIN
     ALTER TABLE dbo.LeaveCategory ADD ModifiedDate DATETIME NULL;
+    PRINT 'Added ModifiedDate to LeaveCategory.';
 END
+ELSE
+    PRINT 'ModifiedDate already exists in LeaveCategory.';
 GO
 
 /* ============================================================
-   2. EMPLOYEE TRIGGER
+   2. EMPLOYEE TRIGGER (create if not exists)
 ============================================================ */
-DROP TRIGGER IF EXISTS dbo.trg_Employee_Audit;
-GO
-
-CREATE TRIGGER dbo.trg_Employee_Audit
-ON dbo.Employee
-AFTER UPDATE
-AS
+IF NOT EXISTS (SELECT * FROM sys.triggers WHERE name = 'trg_Employee_Audit' AND parent_class_desc = 'OBJECT_OR_COLUMN')
 BEGIN
-    SET NOCOUNT ON;
+    EXEC sp_executesql N'
+        CREATE TRIGGER dbo.trg_Employee_Audit
+        ON dbo.Employee
+        AFTER UPDATE
+        AS
+        BEGIN
+            SET NOCOUNT ON;
 
-    DECLARE @BatchID INT = -1;
+            DECLARE @BatchID INT = -1;
 
-    IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
-        SET @BatchID = CONVERT(INT, CONTEXT_INFO());
+            IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
+                SET @BatchID = CONVERT(INT, CONTEXT_INFO());
 
-    UPDATE dbo.Employee
-    SET ModifiedDate = GETDATE()
-    FROM inserted i
-    WHERE dbo.Employee.EmployeeID = i.EmployeeID;
+            UPDATE dbo.Employee
+            SET ModifiedDate = GETDATE()
+            FROM inserted i
+            WHERE dbo.Employee.EmployeeID = i.EmployeeID;
 
-    INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
-    SELECT
-        @BatchID,
-        'Employee',
-        'UPDATE',
-        'SUCCESS',
-        COUNT(*),
-        HOST_NAME(),
-        APP_NAME(),
-        GETDATE()
-    FROM inserted;
+            INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
+            SELECT
+                @BatchID,
+                ''Employee'',
+                ''UPDATE'',
+                ''SUCCESS'',
+                COUNT(*),
+                HOST_NAME(),
+                APP_NAME(),
+                GETDATE()
+            FROM inserted;
+        END
+    ';
+    PRINT 'Trigger trg_Employee_Audit created.';
 END
+ELSE
+    PRINT 'Trigger trg_Employee_Audit already exists – skipped.';
 GO
 
 /* ============================================================
-   3. TIMESHEET TRIGGER
+   3. TIMESHEET TRIGGER (create if not exists)
 ============================================================ */
-DROP TRIGGER IF EXISTS dbo.trg_Timesheet_Audit;
-GO
-
-CREATE TRIGGER dbo.trg_Timesheet_Audit
-ON dbo.Timesheet
-AFTER UPDATE
-AS
+IF NOT EXISTS (SELECT * FROM sys.triggers WHERE name = 'trg_Timesheet_Audit' AND parent_class_desc = 'OBJECT_OR_COLUMN')
 BEGIN
-    SET NOCOUNT ON;
+    EXEC sp_executesql N'
+        CREATE TRIGGER dbo.trg_Timesheet_Audit
+        ON dbo.Timesheet
+        AFTER UPDATE
+        AS
+        BEGIN
+            SET NOCOUNT ON;
 
-    DECLARE @BatchID INT = -1;
+            DECLARE @BatchID INT = -1;
 
-    IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
-        SET @BatchID = CONVERT(INT, CONTEXT_INFO());
+            IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
+                SET @BatchID = CONVERT(INT, CONTEXT_INFO());
 
-    UPDATE dbo.Timesheet
-    SET ModifiedDate = GETDATE()
-    FROM inserted i
-    WHERE dbo.Timesheet.TimesheetID = i.TimesheetID;
+            UPDATE dbo.Timesheet
+            SET ModifiedDate = GETDATE()
+            FROM inserted i
+            WHERE dbo.Timesheet.TimesheetID = i.TimesheetID;
 
-    INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
-    SELECT
-        @BatchID,
-        'Timesheet',
-        'UPDATE',
-        'SUCCESS',
-        COUNT(*),
-        HOST_NAME(),
-        APP_NAME(),
-        GETDATE()
-    FROM inserted;
+            INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
+            SELECT
+                @BatchID,
+                ''Timesheet'',
+                ''UPDATE'',
+                ''SUCCESS'',
+                COUNT(*),
+                HOST_NAME(),
+                APP_NAME(),
+                GETDATE()
+            FROM inserted;
+        END
+    ';
+    PRINT 'Trigger trg_Timesheet_Audit created.';
 END
+ELSE
+    PRINT 'Trigger trg_Timesheet_Audit already exists – skipped.';
 GO
 
 /* ============================================================
-   4. LEAVE TRIGGER
+   4. LEAVE TRIGGER (create if not exists)
 ============================================================ */
-DROP TRIGGER IF EXISTS dbo.trg_Leave_Audit;
-GO
-
-CREATE TRIGGER dbo.trg_Leave_Audit
-ON dbo.[Leave]
-AFTER UPDATE
-AS
+IF NOT EXISTS (SELECT * FROM sys.triggers WHERE name = 'trg_Leave_Audit' AND parent_class_desc = 'OBJECT_OR_COLUMN')
 BEGIN
-    SET NOCOUNT ON;
+    EXEC sp_executesql N'
+        CREATE TRIGGER dbo.trg_Leave_Audit
+        ON dbo.[Leave]
+        AFTER UPDATE
+        AS
+        BEGIN
+            SET NOCOUNT ON;
 
-    DECLARE @BatchID INT = -1;
+            DECLARE @BatchID INT = -1;
 
-    IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
-        SET @BatchID = CONVERT(INT, CONTEXT_INFO());
+            IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
+                SET @BatchID = CONVERT(INT, CONTEXT_INFO());
 
-    UPDATE dbo.[Leave]
-    SET ModifiedDate = GETDATE()
-    FROM inserted i
-    WHERE dbo.[Leave].LeaveID = i.LeaveID;
+            UPDATE dbo.[Leave]
+            SET ModifiedDate = GETDATE()
+            FROM inserted i
+            WHERE dbo.[Leave].LeaveID = i.LeaveID;
 
-    INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
-    SELECT
-        @BatchID,
-        'Leave',
-        'UPDATE',
-        'SUCCESS',
-        COUNT(*),
-        HOST_NAME(),
-        APP_NAME(),
-        GETDATE()
-    FROM inserted;
+            INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
+            SELECT
+                @BatchID,
+                ''Leave'',
+                ''UPDATE'',
+                ''SUCCESS'',
+                COUNT(*),
+                HOST_NAME(),
+                APP_NAME(),
+                GETDATE()
+            FROM inserted;
+        END
+    ';
+    PRINT 'Trigger trg_Leave_Audit created.';
 END
+ELSE
+    PRINT 'Trigger trg_Leave_Audit already exists – skipped.';
 GO
 
 /* ============================================================
-   5. LEAVE CATEGORY TRIGGER
+   5. LEAVE CATEGORY TRIGGER (create if not exists)
 ============================================================ */
-DROP TRIGGER IF EXISTS dbo.trg_LeaveCategory_Audit;
-GO
-
-CREATE TRIGGER dbo.trg_LeaveCategory_Audit
-ON dbo.LeaveCategory
-AFTER UPDATE
-AS
+IF NOT EXISTS (SELECT * FROM sys.triggers WHERE name = 'trg_LeaveCategory_Audit' AND parent_class_desc = 'OBJECT_OR_COLUMN')
 BEGIN
-    SET NOCOUNT ON;
+    EXEC sp_executesql N'
+        CREATE TRIGGER dbo.trg_LeaveCategory_Audit
+        ON dbo.LeaveCategory
+        AFTER UPDATE
+        AS
+        BEGIN
+            SET NOCOUNT ON;
 
-    DECLARE @BatchID INT = -1;
+            DECLARE @BatchID INT = -1;
 
-    IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
-        SET @BatchID = CONVERT(INT, CONTEXT_INFO());
+            IF CONTEXT_INFO() IS NOT NULL AND CONTEXT_INFO() != 0x00000000
+                SET @BatchID = CONVERT(INT, CONTEXT_INFO());
 
-    UPDATE dbo.LeaveCategory
-    SET ModifiedDate = GETDATE()
-    FROM inserted i
-    WHERE dbo.LeaveCategory.LeaveCategoryID = i.LeaveCategoryID;
+            UPDATE dbo.LeaveCategory
+            SET ModifiedDate = GETDATE()
+            FROM inserted i
+            WHERE dbo.LeaveCategory.LeaveCategoryID = i.LeaveCategoryID;
 
-    INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
-    SELECT
-        @BatchID,
-        'LeaveCategory',
-        'UPDATE',
-        'SUCCESS',
-        COUNT(*),
-        HOST_NAME(),
-        APP_NAME(),
-        GETDATE()
-    FROM inserted;
+            INSERT INTO dbo.AuditLog (BatchID, TableName, OperationType, StatusCode, RowsUpdated, HostName, ApplicationName, EventDateTime)
+            SELECT
+                @BatchID,
+                ''LeaveCategory'',
+                ''UPDATE'',
+                ''SUCCESS'',
+                COUNT(*),
+                HOST_NAME(),
+                APP_NAME(),
+                GETDATE()
+            FROM inserted;
+        END
+    ';
+    PRINT 'Trigger trg_LeaveCategory_Audit created.';
 END
-GO
-
-/* ============================================================
-   6. VERIFY
-============================================================ */
-SELECT
-    t.name AS TriggerName,
-    OBJECT_NAME(t.parent_id) AS TableName,
-    CASE WHEN t.is_disabled = 0 THEN 'ACTIVE' ELSE 'DISABLED' END AS Status
-FROM sys.triggers t
-WHERE t.parent_class = 0
-AND t.name LIKE 'trg_%_Audit'
-ORDER BY TableName;
+ELSE
+    PRINT 'Trigger trg_LeaveCategory_Audit already exists – skipped.';
 GO
